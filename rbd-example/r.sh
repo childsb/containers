abe6e27f099033c89cbe4afc4a698f5b1a4eecc0
bradley childs <bchilds@redhat.com>
2016-01-30 20:41:35 -0600

    added nginx example

--- rbd-example/r.sh
+++ rbd-example/r.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+kubectl.sh delete rc rbd-tester
+kubectl.sh delete pvc myclaim
+
+
 kubectl.sh create -f secret.yaml
-kubectl.sh create -f rbd.yaml
+
+kubectl.sh create -f pv.yaml
+
+# kubectl.sh create -f rbd.yaml
+kubectl.sh create -f nginx.yaml
--- rbd-volume/r.sh
+++ rbd-volume/r.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# docker build -t rbd:latest .
-docker run --privileged=true -t rbd-volume:latest
+docker build -t rbd:latest .
+docker run --privileged=true -t rbd:latest
