4edf7d414029943df80dce8aa074c34846b6c462
childsb <bchilds@redhat.com>
2016-07-22 21:13:57 -0400

    updated bs.sh to use rebase instead of merge

--- bs.sh
+++ bs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git config --global user.name "childsb"
 git config --global user.email "bchilds@redhat.com"
-git config --global alias.pu '!git fetch origin -v; git fetch upstream -v; git merge upstream/master'
+git config --global alias.pu '!git fetch origin -v; git fetch upstream -v; git rebase upstream/master'
 git remote set-url --push upstream no_push
 git config --global push.default simple
