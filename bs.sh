d2be771aa14b1d36a3d7041fa2e653fd4b664e94
childsb <bchilds@redhat.com>
2017-11-01 22:34:03 -0400

    add some packages

--- bs.sh
+++ bs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+yum install -y wget
+cd /usr/sbin
+wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+mv jq-linux64 jq
 git config --global user.name "childsb"
 git config --global user.email "bchilds@redhat.com"
 git config --global alias.pu '!git fetch origin -v; git fetch upstream -v; git rebase upstream/master'
